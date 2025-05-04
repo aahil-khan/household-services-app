2db16172c99ad459bf5a63e46b684cfc8fc737c8
aahil-khan <aahilminookhan@gmail.com>
2025-05-05 02:39:43 +0530

    init pg

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ from datetime import datetime
 current_dir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__, template_folder="templates")
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.join(current_dir , "database/household-services-app.db")
+#app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.join(current_dir , "database/household-services-app.db")
 
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:pgadmin@localhost/household-services-app'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:pgadmin@localhost/household-services-app'
 
 db.init_app(app)
 app.app_context().push()
--- application/models.py
+++ application/models.py
@@ -3,14 +3,14 @@ from datetime import datetime
 
 
 class User(db.Model):
-    __tablename__ = 'Users'
+    __tablename__ = 'users'
     
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
     name = db.Column(db.String, nullable=False)
     role = db.Column(db.String, nullable=False)
-    service_id = db.Column(db.Integer, db.ForeignKey('Services.id' , ondelete = 'CASCADE'), nullable=True)
+    service_id = db.Column(db.Integer, db.ForeignKey('services.id' , ondelete = 'CASCADE'), nullable=True)
     category_id = db.Column(db.Integer, db.ForeignKey('service_category.id' , ondelete = 'CASCADE'), nullable=True)
     experience = db.Column(db.Integer, nullable=True)
     approval_status = db.Column(db.String, nullable=True)
@@ -25,7 +25,7 @@ class User(db.Model):
 
 
 class Service(db.Model):
-    __tablename__ = 'Services'
+    __tablename__ = 'services'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String, nullable=False, unique=True)
     base_price = db.Column(db.Float, nullable=False)
@@ -40,10 +40,11 @@ class ServiceCategory(db.Model):
     name = db.Column(db.String(100), unique=True, nullable=False)
     description = db.Column(db.Text)
 
+
 class Review(db.Model):
-    __tablename__ = 'Review'
+    __tablename__ = 'review'
     id = db.Column(db.Integer, primary_key=True)
-    servicereq_id = db.Column(db.Integer, db.ForeignKey('Services_requests.id'), nullable=False)
+    servicereq_id = db.Column(db.Integer, db.ForeignKey('services_requests.id'), nullable=False)
     rating = db.Column(db.Integer, nullable=False)
     comment = db.Column(db.Text)
     created_at = db.Column(db.DateTime, default=datetime.now)
@@ -55,9 +56,9 @@ class Review(db.Model):
 
 
 class Wallet(db.Model):
-    __tablename__ = 'Wallet'
+    __tablename__ = 'wallet'
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('Users.id'), nullable=False, unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False, unique=True)
     balance = db.Column(db.Float, default=10000.0)
     updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
 
@@ -68,7 +69,7 @@ class Wallet(db.Model):
 
 
 class Coupon(db.Model):
-    __tablename__ = 'Coupon'
+    __tablename__ = 'coupon'
     id = db.Column(db.Integer, primary_key=True)
     code = db.Column(db.String(20), unique=True, nullable=False)
     discount_percent = db.Column(db.Float, nullable=False)
@@ -77,23 +78,24 @@ class Coupon(db.Model):
     max_uses = db.Column(db.Integer)
     current_uses = db.Column(db.Integer, default=0)
 
+
 class Payment(db.Model):
-    __tablename__ = 'Payment'
+    __tablename__ = 'payment'
     id = db.Column(db.Integer, primary_key=True)
-    customer_id = db.Column(db.Integer, db.ForeignKey('Users.id' , ondelete = 'CASCADE'), nullable=True)
-    professional_id = db.Column(db.Integer, db.ForeignKey('Users.id' , ondelete = 'CASCADE'), nullable=True)
-    servicereq_id = db.Column(db.Integer, db.ForeignKey('Services_requests.id' , ondelete = 'CASCADE'), nullable=True)
+    customer_id = db.Column(db.Integer, db.ForeignKey('users.id' , ondelete = 'CASCADE'), nullable=True)
+    professional_id = db.Column(db.Integer, db.ForeignKey('users.id' , ondelete = 'CASCADE'), nullable=True)
+    servicereq_id = db.Column(db.Integer, db.ForeignKey('services_requests.id' , ondelete = 'CASCADE'), nullable=True)
     amount = db.Column(db.Float, nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.now)
 
 
 class ServiceRequest(db.Model):
-    __tablename__ = 'Services_requests'
+    __tablename__ = 'services_requests'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    service_id = db.Column(db.Integer, db.ForeignKey('Services.id', ondelete = 'CASCADE'), nullable=False)
-    customer_id = db.Column(db.Integer, db.ForeignKey('Users.id' , ondelete = 'CASCADE'), nullable=True)
-    professional_id = db.Column(db.Integer, db.ForeignKey('Users.id' , ondelete = 'CASCADE'), nullable=True)
+    service_id = db.Column(db.Integer, db.ForeignKey('services.id', ondelete = 'CASCADE'), nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey('users.id' , ondelete = 'CASCADE'), nullable=True)
+    professional_id = db.Column(db.Integer, db.ForeignKey('users.id' , ondelete = 'CASCADE'), nullable=True)
     date_of_request = db.Column(db.String, nullable=True)
     date_of_completion = db.Column(db.String, nullable=True)
     status = db.Column(db.String, default='requested')
